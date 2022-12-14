02806c94739fbf256bc06d24bc63250f4c7acf99
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-12-14 17:00:08 -0300

    readFile.js created and req 01 done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+const { readFile } = require('./readFile');
 
 const app = express();
 app.use(express.json());
@@ -6,9 +8,16 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const pathName = path.resolve(__dirname, './talker.json');
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+ 
+app.get('/talker', async (_req, res) => {
+   const talker = await readFile(pathName);
+   res.status(HTTP_OK_STATUS).json(talker);
 });
 
 app.listen(PORT, () => {
